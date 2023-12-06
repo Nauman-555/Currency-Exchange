36ff5718ef9449f6312e700fb5c4b84d39373302
Nauman-555 <naumannzaib@gmail.com>
2023-12-06 23:10:36 +0500

    06/12/2023

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,25 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 const App = () => {
+  const [rate, setRate] = useState(null);
+  const [amount, setAmount] = useState('')
   const [base, setBase] = useState('');
   const [target, setTarget] = useState('');
-  const [rate, setRate] = useState(null);
-
-  const handleClickConvert = () => {
-    if (!base || !target) {
-      alert('Please fill all blocks');
-      return;
-    }
+  const handleOptionChange = (event) => {
+  setBase(event.target.value);
+  // setAmount('');
+  setRate(null);
+};
+const handleOptionChange2 = (event) => {
+  setTarget(event.target.value);
+  // setAmount('');
+  setRate(null);
+};
+const handleAmountChange = (event) => {
+  setAmount(event.target.value);
+};
+const handleClickConvert = () => {
+  if (base && target && amount) {
     setRate(null);
     axios
       .get(
@@ -23,47 +33,133 @@ const App = () => {
       .catch((error) => {
         console.log(error);
       });
-
-    setBase('');
-    setTarget('');
+  } else {
+    alert('Please fill in all the fields before converting.');
+  }
+};
+  const calculateConvertedAmount = () => {
+    if (rate && !isNaN(amount) && amount !== '') {
+      const exchangeRate = rate[target];
+      if (exchangeRate !== undefined) {
+        const convertedAmount = amount * exchangeRate;
+        return  convertedAmount.toFixed(2);
+      } 
+    } 
   };
-
   return (
     <div>
       <h1>Currency Exchange Rates</h1>
-      <input
-        type="text"
-        placeholder="Enter source"
-        value={base}
-        maxLength={3}
-        onChange={(e) => {
-          setBase(e.target.value);
-        }}
-      />
-      <input
-        type="text"
-        placeholder="Enter target"
-        value={target}
-        maxLength={3}
-        onChange={(e) => {
-          setTarget(e.target.value);
-        }}
-      />
+    <div className='fan'>
+      <div>
+      <label htmlFor="selectBase">Select an Source:</label>
+       <select id="selectBase" value={base} onChange={handleOptionChange}>
+        <option value="">Select...</option>
+        <option value="ARS">ARS</option>
+        <option value="AUD">AUD</option>
+        <option value="BCH">BCH</option>
+        <option value="BGN">BGN</option>
+        <option value="BNB">BNB</option>
+        <option value="BRL">BRL</option>
+        <option value="BTC">BTC</option>
+        <option value="CAD">CAD</option>
+        <option value="CHF">CHF</option>
+        <option value="CNY">CNY</option>
+        <option value="CZK">CZK</option>
+        <option value="DKK">DKK</option>
+        <option value="DOGE">DOGE</option>
+        <option value="DZD">DZD</option>
+        <option value="ETH">ETH</option>
+        <option value="EUR">EUR</option>
+        <option value="GBP">GBP</option>
+        <option value="HKD">HKD</option>
+        <option value="HRK">HRK</option>
+        <option value="HUF">HUF</option>
+        <option value="IDR">IDR</option>
+        <option value="ILS">ILS</option>
+        <option value="INR">INR</option>
+        <option value="ISK">ISK</option>
+        <option value="JPY">JPY</option>
+        <option value="KRW">KRW</option>
+        <option value="LTC">LTC</option>
+        <option value="MAD">MAD</option>
+        <option value="MXN">MXN</option>
+        <option value="MYR">MYR</option>
+        <option value="NOK">NOK</option>
+        <option value="NZD">NZD</option>
+        <option value="PHP">PHP</option>
+        <option value="PLN">PLN</option>
+        <option value="RON">RON</option>
+        <option value="RUB">RUB</option>
+        <option value="SEK">SEK</option>
+        <option value="SGD">SGD</option>
+        <option value="THB">THB</option>
+        <option value="TRY">TRY</option>
+        <option value="TWD">TWD</option>
+        <option value="XRP">XRP</option>
+        <option value="ZAR">ZAR</option>
+        <option value="USD">USD</option> 
+      </select>
+      <label htmlFor="selectOption">Select a Target:</label>
+       <select id="selectOption2" value={target} onChange={handleOptionChange2}>
+        <option value="">Select...</option>
+        <option value="ARS">ARS</option>
+        <option value="AUD">AUD</option>
+        <option value="BCH">BCH</option>
+        <option value="BGN">BGN</option>
+        <option value="BNB">BNB</option>
+        <option value="BRL">BRL</option>
+        <option value="BTC">BTC</option>
+        <option value="CAD">CAD</option>
+        <option value="CHF">CHF</option>
+        <option value="CNY">CNY</option>
+        <option value="CZK">CZK</option>
+        <option value="DKK">DKK</option>
+        <option value="DOGE">DOGE</option>
+        <option value="DZD">DZD</option>
+        <option value="ETH">ETH</option>
+        <option value="EUR">EUR</option>
+        <option value="GBP">GBP</option>
+        <option value="HKD">HKD</option>
+        <option value="HRK">HRK</option>
+        <option value="HUF">HUF</option>
+        <option value="IDR">IDR</option>
+        <option value="ILS">ILS</option>
+        <option value="INR">INR</option>
+        <option value="ISK">ISK</option>
+        <option value="JPY">JPY</option>
+        <option value="KRW">KRW</option>
+        <option value="LTC">LTC</option>
+        <option value="MAD">MAD</option>
+        <option value="MXN">MXN</option>
+        <option value="MYR">MYR</option>
+        <option value="NOK">NOK</option>
+        <option value="NZD">NZD</option>
+        <option value="PHP">PHP</option>
+        <option value="PLN">PLN</option>
+        <option value="RON">RON</option>
+        <option value="RUB">RUB</option>
+        <option value="SEK">SEK</option>
+        <option value="SGD">SGD</option>
+        <option value="THB">THB</option>
+        <option value="TRY">TRY</option>
+        <option value="TWD">TWD</option>
+        <option value="XRP">XRP</option>
+        <option value="ZAR">ZAR</option>
+        <option value="USD">USD</option>
+      </select>
+      </div>
+      <br></br>
+      <input type="number" value={amount} placeholder='  Enter amount' onChange={handleAmountChange} />
       <br />
       <button className='but' onClick={handleClickConvert}>Convert</button>
+    {rate && typeof rate === 'object' && base && target && amount ? (
+  <div>
+    <h2> {calculateConvertedAmount()} {target} </h2>
+  </div>
+) : null}
 
-      {rate && typeof rate === 'object' ? (
-        <div>
-          <h2>Exchange Rate:</h2>
-          {Object.keys(rate).map((key) => (
-            <div key={key}>
-              <h2>{rate[key]}</h2>
-            </div>
-          ))}
-        </div>
-      ) : null}
+    </div>
     </div>
   );
 };
-
 export default App;
